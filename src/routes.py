e6b88c1a12be781e07aae5f0f9a5b6c38167589e
heylarissa <larissah.dandrade@gmail.com>
2025-02-12 18:44:10 -0300

    cria estrutura

---
+++ src/routes.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, jsonify
+
+api_bp = Blueprint("api", __name__)
+
+@api_bp.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "Welcome to the API!"})
+
+def register_routes(app):
+    app.register_blueprint(api_bp)
